52046b70055df71171488a7ca479c70f11605c06
_nguyen.du <1851061329@e.tlu.edu.vn>
2023-03-02 13:51:57 +0700

    Checkout logic

--- app/views.py
+++ app/views.py
@@ -7,6 +7,7 @@ def home(request):
     products = Product.objects.all()
     context={'products':products}
     return render(request, 'app/home.html', context)
+
 def cart(request):
     if request.user.is_authenticated:
         customer = request.user.customer
@@ -14,8 +15,17 @@ def cart(request):
         items = order.orderitem_set.all()
     else:
         items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
     context= {'items':items, 'order':order}
     return render(request, 'app/cart.html', context)
+
 def checkout(request):
-    context= {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+    context= {'items':items, 'order':order}
     return render(request, 'app/checkout.html', context)
