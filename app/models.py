3d0e244ac369b4dc057954e26283b21b6a44fa89
_nguyen.du <1851061329@e.tlu.edu.vn>
2023-03-02 13:16:39 +0700

    Read data

--- app/models.py
+++ app/models.py
@@ -14,28 +14,36 @@ class Product(models.Model):
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField()
     digital = models.BooleanField(default=False, null=True, blank=False)
+    image = models.ImageField(null=True, blank=True)
     
     def __str__(self):
         return self.name
-    
-class Oder(models.Model):
+    @property
+    def ImageUrl(seft):
+        try: 
+            url = seft.image.url
+        except:
+            url = ''
+        return url
+
+class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
-    date_oder = models.DateTimeField(auto_now_add=True)
+    date_order = models.DateTimeField(auto_now_add=True)
     complete = models.BooleanField(default=False, null=True, blank=False)
     transction_id = models.CharField(max_length=200, null=True)
     
     def __str__(self):
         return str(self.id)
     
-class OderItem(models.Model):
+class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, blank=True)
-    oder = models.ForeignKey(Oder, on_delete=models.SET_NULL, null=True, blank=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True, blank=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
     
 class ShippingAddress(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
-    oder = models.ForeignKey(Oder, on_delete=models.SET_NULL, null=True, blank=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True, blank=True)
     address = models.CharField(max_length=200, null=True)
     city = models.CharField(max_length=200, null=True)
     state = models.CharField(max_length=200, null=True)
