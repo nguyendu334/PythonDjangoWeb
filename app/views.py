ef69b0e2910eec2d3687c013d7af60456dea0f55
_nguyen.du <1851061329@e.tlu.edu.vn>
2023-03-03 10:24:54 +0700

    Add to cart #2

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .models import *
+import json
 
 # Create your views here.
 def home(request):
@@ -29,3 +30,20 @@ def checkout(request):
         order = {'get_cart_total':0, 'get_cart_items':0}
     context= {'items':items, 'order':order}
     return render(request, 'app/checkout.html', context)
+
+def updateItem(request):
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+    customer = request.user.customer
+    product = Product.objects.get(id = productId)
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+    orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+    if action == 'add':
+        orderItem.quantity += 1
+    elif action == 'remove':
+        orderItem.quantity -= 1
+    orderItem.save()
+    if orderItem.quantity <= 0:
+        orderItem.delete()
+    return JsonResponse('Item was added', safe=False)
